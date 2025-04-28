3db381d2325c8f5c3e651fb1cdf51baace4b3f3b
Ilyosbek <ilyosbekibroximov23z@gmail.com>
2025-04-28 10:36:12 +0500

    global-messag update

--- src/global-message/global-message.controller.ts
+++ src/global-message/global-message.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
 import { GlobalMessageService } from './global-message.service';
 import { CreateGlobalMessageDto } from './dto/create-global-message.dto';
 import { UpdateGlobalMessageDto } from './dto/update-global-message.dto';
 import { Request } from 'express';
+import { AuthorizationGuard } from 'src/authorization/authorization.guard';
 
 @Controller('global-message')
 export class GlobalMessageController {
   constructor(private readonly globalMessageService: GlobalMessageService) {}
 
+  @UseGuards(AuthorizationGuard)
   @Post()
   create(@Body() createGlobalMessageDto: CreateGlobalMessageDto, @Req() req: Request) {
     return this.globalMessageService.create(createGlobalMessageDto, req);
@@ -18,11 +20,13 @@ export class GlobalMessageController {
     return this.globalMessageService.findAll();
   }
 
+  @UseGuards(AuthorizationGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGlobalMessageDto: UpdateGlobalMessageDto) {
     return this.globalMessageService.update(id, updateGlobalMessageDto);
   }
 
+  @UseGuards(AuthorizationGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.globalMessageService.remove(id);
